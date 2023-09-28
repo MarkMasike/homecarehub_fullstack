b5bebc8934fa34284d3b113b70869d2826312db1
MarkMasike <marksoko002@gmail.com>
2023-09-28 05:48:53 +0300

    commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,16 @@
+import User from "../models/user.model.js";
+
 export const register= async(req,res)=>{
    
     try{
+      const newUser = new User({
+        username:"test",
+        email:"test",
+        password:"test",
+        country:"test",
+      });
+      await newUser.save();
+      res.status(201).send("User created successfully");
 
     }catch(err){
         res.status(500).send('something went wrong');
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import {register,login,logout} from "../controllers/auth.controller"
+import {register,login,logout} from "../controllers/auth.controller.js"
 const router = express.Router();
 
 router.post("/register",register);
-router.post("/register",login);
+router.post("/login",login);
 router.post("/logout",logout);
 
 export default router;
