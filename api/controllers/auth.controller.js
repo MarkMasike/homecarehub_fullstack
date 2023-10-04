01dbd84321faf2412290925a0a316bc9a8b41c24
MarkMasike <markmasike79@gmail.com>
2023-10-04 19:06:12 +0300

    auth commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,12 @@
 import User from "../models/user.model.js";
-
+import bcrypt from 'bcrypt';
 export const register= async(req,res)=>{
    
     try{
+      const hash = bcrypt.hashSync(req.body.password,5);
       const newUser = new User({
-        username:"test",
-        email:"test",
-        password:"test",
-        country:"test",
+        ...req.body,
+        password:hash,
       });
       await newUser.save();
       res.status(201).send("User created successfully");
@@ -16,9 +15,20 @@ export const register= async(req,res)=>{
         res.status(500).send('something went wrong');
     }
 }
-export const login=(res,req)=>{
-   
+export const login = async (req,res)=>{
+   try{
+     const user = await User.findOne({username:req.body.username});
+     if (!user) return res.status(404).send("User not found!");
+
+     const isCorrect = bcrypt.compareSync(req.body.password,user.password);
+     if (!isCorrect) return res.status(400).send("Incorrect username of password!");
+     
+     const {password, ...info}=user;
+     res.status(200).send(info);
+   }catch(err){
+        // res.status(500).send('something went wrong');
+    }
 }
-export const logout =(res,req)=>{
+export const logout =(req,res)=>{
    
 }
--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ const connect = async()=>{
         console.log(error);
     }
 }
-
+app.use(express.json());
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
 app.use("/api/gigs",gigRoute);
